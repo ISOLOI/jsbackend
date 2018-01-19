956d4fee1f9d3cf622d590d2bffb508e3f6eaa32
ISOLOI <nicholasjkaufmann2gmail.com>
2018-01-19 12:23:24 -0500

    mlab connection

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const mongoose = require('mongoose');
 const MeetupController =  require('./src/controllers/meetup');
 const config = require('./config.js'); // remove .example from /server/config.js.example
 //const MongoDBUrl = 'mongodb://localhost:27017/meetupapi';
-const MongoDBUrl = 'mongodb://unicodeveloper:pote1142@ds235827.mlab.com:35827/meetup';
-
+const MongoDBUrl = 'mongodb://coding-saints:952015start@ds029338.mlab.com:29338/meet';
+var port = 3333 || process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -35,7 +35,7 @@ app.post('/api/meetups', MeetupController.create);
 app.get('/api/meetups/public', MeetupController.getPublicMeetups);
 app.get('/api/meetups/private', authCheck, MeetupController.getPrivateMeetups);
 
-app.listen(3333 || process.env.PORT);
-console.log('Listening on localhost:3333');
+app.listen(port);
+console.log('Listening on localhost');
  // Once started, connect to Mongo through Mongoose
 mongoose.connect(MongoDBUrl, {}).then(() => { console.log(`Connected to Mongo server`) }, err => { console.log(err) });
